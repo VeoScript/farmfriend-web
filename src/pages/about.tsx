2763739104e25d8d240c518f54e1f6676420a599
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-04-09 13:14:11 +0800

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -13,9 +13,9 @@ const About = () => {
         <div className="flex flex-col items-center justify-center w-full h-full p-10 space-y-5">
           <h1 className="font-bold text-2xl text-olive-dark">THE TEAM</h1>
           <p className="w-full max-w-xl font-light text-sm text-center">
-            "Teamwork makes the dream work! When we work together, we can achieve so much more than we could ever accomplish alone.
+            {`"Teamwork makes the dream work! When we work together, we can achieve so much more than we could ever accomplish alone.
             By combining our strengths, supporting each other through our weaknesses, and celebrating our successes as a team, we can reach new heights and conquer any challenge that comes our way.
-            So let's roll up our sleeves, put our heads together, and show the world what we're capable of when we work as one!"
+            So let's roll up our sleeves, put our heads together, and show the world what we're capable of when we work as one!"`}
           </p>
           <div className="flex flex-row items-center w-full space-x-5">
             <div className="flex flex-col items-center w-full p-5 shadow-xl rounded-xl border border-olive-semi-light bg-yellow-green bg-opacity-50">
