428047ece3a680debf33cfc0843e9325ac5d706d
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-04-09 15:12:39 +0800

    feat: add responsive layout in login page (#15)

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -58,38 +58,38 @@ const Login: NextPage<IProps> = ({ cookies }) => {
         <title>FarmFriend</title>
       </Head>
       <MainLayout>
-        <div className="flex flex-row items-center justify-between w-full max-w-6xl h-full space-x-20">
-          <div className="flex flex-row items-center w-full">
+        <div className="flex flex-col md:flex-row items-center justify-between w-full max-w-full md:max-w-6xl h-full space-y-10 md:space-x-20">
+          <div className="flex flex-col md:flex-row items-center md:items-center justify-center md:justify-start w-full p-5 md:p-0">
             <Image
               src={'/images/farmfriend-logo.png'}
               width={500}
               height={500}
-              className="-mt-10 w-[20rem] h-[20rem] rounded-full object-cover"
+              className="m-0 md:-mt-10 w-[10rem] h-[10rem] md:w-[20rem] md:h-[20rem] rounded-full object-cover"
               quality={100}
               alt="Profile"
               blurDataURL={'/images/farmfriend-logo.png'}
               placeholder="blur"
             />
-            <div className="flex flex-col w-full space-y-3">
-              <h2 className="font-bold text-2xl text-olive">A weather support application for farmers.</h2>
-              <h3 className="text-lg text-olive">Growing Stronger Together: Supporting Agriculture for a Sustainable Future.</h3>
-              <Link href={'https://github.com/VeoScript/FarmFriend/releases/download/version-1.0/farmfriend.apk'} className="flex flex-row items-center justify-center w-[18rem] px-5 py-3 space-x-3 rounded-xl bg-yellow-green transition ease-in-out duration-200 hover:bg-opacity-50">
-                <span className="text-sm">Download the app for Android</span>
+            <div className="flex flex-col items-center md:items-start w-full space-y-3">
+              <h2 className="font-bold text-xl md:text-2xl text-center md:text-left text-olive">A weather support application for farmers.</h2>
+              <h3 className="text-base md:text-lg text-center md:text-left text-olive">Growing Stronger Together: Supporting Agriculture for a Sustainable Future.</h3>
+              <Link href={'/apk/farmfriend.apk'} className="flex flex-row items-center justify-center w-[18rem] px-5 py-3 space-x-3 rounded-xl bg-yellow-green transition ease-in-out duration-200 hover:bg-opacity-50">
+                <span className="text-xs md:text-sm">Download the app for Android</span>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
                 </svg>
               </Link>
             </div>
           </div>
-          <form onSubmit={handleLogin} className="flex flex-col items-center w-full max-w-sm space-y-5">
-            <h2 className="font-bold text-2xl text-olive">Welcome Back!</h2>
+          <form onSubmit={handleLogin} className="flex flex-col items-center w-full max-w-sm py-5 md:py-0 space-y-5">
+            <h2 className="font-bold text-xl md:text-2xl text-olive">Welcome Back!</h2>
             {error && (
               <div className="flex flex-row items-center justify-center w-full p-3 rounded-xl bg-red-500 bg-opacity-50">
                 <h3 className="font-thin text-sm text-white">{ error }</h3>
               </div>
             )}
             <div className="flex flex-col w-full space-y-2">
-              <label htmlFor="email" className="ml-3 text-sm text-neutral-400">Email</label>
+              <label htmlFor="email" className="ml-3 text-xs md:text-sm text-neutral-400">Email</label>
               <input
                 id="email"
                 type="email"
@@ -103,7 +103,7 @@ const Login: NextPage<IProps> = ({ cookies }) => {
               />
             </div>
             <div className="flex flex-col w-full space-y-2">
-              <label htmlFor="password" className="ml-3 text-sm text-neutral-400">Password</label>
+              <label htmlFor="password" className="ml-3 text-xs md:text-sm text-neutral-400">Password</label>
               <input
                 id="password"
                 type="password"
